bc31e441f46b18b504c5e69f4953964005897451
Bogdan Cristea <cristeab@gmail.com>
2022-10-02 22:05:52 +0300

    all deps are installed in the same folder

--- tools/setup-dev-env.ps1
+++ tools/setup-dev-env.ps1
@@ -1,5 +1,6 @@
 $PJSIP_ARCHIVE = '2.12.1.zip'
 $OPENH264_ARCHIVE = 'openh264-2.3.1-win64.dll.bz2'
+$INSTALL_DIR = deps
 
 Write-Output "Download $PJSIP_ARCHIVE..."
 Invoke-WebRequest -Uri "https://github.com/pjsip/pjproject/archive/refs/tags/$PJSIP_ARCHIVE" -OutFile $PJSIP_ARCHIVE
@@ -8,8 +9,13 @@ Expand-Archive -Force $PJSIP_ARCHIVE .
 Write-Output 'Clone BCG729...'
 git clone https://github.com/BelledonneCommunications/bcg729.git
 
+Write-Output 'Compile BCG729...'
+Copy-Item "build-bcg729.bat" -Destination "bcg729"
+Set-Location bcg729
+.\build-bcg729.bat '$PSScriptRoot/$INSTALL_DIR'
+
 Write-Output 'Download vpx, opus and SDL2 libraries with conan'
-conan install . -if conan
+conan install . -if $INSTALL_DIR
 
 Write-Output "Download $OPENH264_ARCHIVE..."
 Invoke-WebRequest -Uri "http://ciscobinary.openh264.org/$OPENH264_ARCHIVE" -OutFile $OPENH264_ARCHIVE
