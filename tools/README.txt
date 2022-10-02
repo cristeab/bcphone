20b03adb30f095638b70c51aca3a9ccd53a3fa23
Bogdan Cristea <cristeab@gmail.com>
2022-10-02 20:27:00 +0300

    add all win deps

---
+++ tools/README.txt
@@ -0,0 +1,12 @@
+# macOS
+
+- build-pjsip-macos.sh: build PJSIP library from sources
+
+- gen-icns.sh: generate icns file
+
+# Windows
+
+- setup-dev-env.ps1: download PSJIP sources and the associated external dependences.
+Before using this file install chocolatey from https://chocolatey.org, then run
+
+    choco install conan 7zip
--- tools/setup-dev-env.ps1
+++ tools/setup-dev-env.ps1
@@ -1,4 +1,5 @@
 $PJSIP_ARCHIVE = '2.12.1.zip'
+$OPENH264_ARCHIVE = 'openh264-2.3.1-win64.dll.bz2'
 
 Write-Output 'Download $PJSIP_ARCHIVE...'
 Invoke-WebRequest -Uri "https://github.com/pjsip/pjproject/archive/refs/tags/$PJSIP_ARCHIVE" -OutFile $PJSIP_ARCHIVE
@@ -7,5 +8,9 @@ Expand-Archive -Force $PJSIP_ARCHIVE .
 Write-Output 'Clone BCG729...'
 git clone https://github.com/BelledonneCommunications/bcg729.git
 
-Write-Output 'Download libvpx with conan'
-conan install . -if libvpx-info -g txt
+Write-Output 'Download libvpx with conan, then copy include and lib folders using the paths found in conanbuildinfo.txt'
+conan install . -if conan-build-info -g txt -g cmake
+
+Write-Output 'Download $OPENH264_ARCHIVE...'
+Invoke-WebRequest -Uri "http://ciscobinary.openh264.org/$OPENH264_ARCHIVE" -OutFile $OPENH264_ARCHIVE
+7z x -y $OPENH264_ARCHIVE
