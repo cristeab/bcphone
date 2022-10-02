b76c6792e3f12fae9458e0a4258221aa11fa5b53
Bogdan Cristea <cristeab@gmail.com>
2022-10-02 21:39:32 +0300

    build script for G729

--- tools/setup-dev-env.ps1
+++ tools/setup-dev-env.ps1
@@ -1,16 +1,16 @@
 $PJSIP_ARCHIVE = '2.12.1.zip'
 $OPENH264_ARCHIVE = 'openh264-2.3.1-win64.dll.bz2'
 
-Write-Output 'Download $PJSIP_ARCHIVE...'
+Write-Output "Download $PJSIP_ARCHIVE..."
 Invoke-WebRequest -Uri "https://github.com/pjsip/pjproject/archive/refs/tags/$PJSIP_ARCHIVE" -OutFile $PJSIP_ARCHIVE
 Expand-Archive -Force $PJSIP_ARCHIVE .
 
 Write-Output 'Clone BCG729...'
 git clone https://github.com/BelledonneCommunications/bcg729.git
 
-Write-Output 'Download external dependences with conan'
+Write-Output 'Download vpx, opus and SDL2 libraries with conan'
 conan install . -if conan
 
-Write-Output 'Download $OPENH264_ARCHIVE...'
+Write-Output "Download $OPENH264_ARCHIVE..."
 Invoke-WebRequest -Uri "http://ciscobinary.openh264.org/$OPENH264_ARCHIVE" -OutFile $OPENH264_ARCHIVE
 7z x -y $OPENH264_ARCHIVE
