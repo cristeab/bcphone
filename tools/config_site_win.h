60b5939339c0229b89f0a169c6b542454c33e927
Bogdan Cristea <cristeab@gmail.com>
2022-10-16 18:38:09 +0300

    link BCG729 lib as static

--- tools/config_site_win.h
+++ tools/config_site_win.h
@@ -7,6 +7,7 @@
 #define PJMEDIA_VIDEO_DEV_SDL_HAS_OPENGL    1
 #define PJMEDIA_HAS_OPUS_CODEC  1
 #define PJMEDIA_HAS_BCG729 1
+#define BCG729_STATIC
 #define PJ_HAS_SSL_SOCK 1
 
 #include "config_site_sample.h"
