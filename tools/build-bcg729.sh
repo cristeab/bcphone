5e176699fc36d104b0d6dfc1f10919d10c485832
Bogdan Cristea <cristeab@gmail.com>
2022-10-16 20:55:56 +0300

    corr build script

--- tools/build-bcg729.sh
+++ tools/build-bcg729.sh
@@ -3,14 +3,15 @@
 set -e
 
 INSTALL_DIR=../bcg729-install
+CMAKE_DIR=/Applications/CMake.app/Contents/bin
 
 mkdir cmake-build
 pushd cmake-build
 
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DENABLE_TESTS=OFF
+$CMAKE_DIR/cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR -DENABLE_SHARED=OFF -DENABLE_STATIC=ON -DENABLE_TESTS=OFF
 
-cmake --build .
+$CMAKE_DIR/cmake --build .
 
-cmake --build . --target install
+$CMAKE_DIR/cmake --build . --target install
 
 popd
